61a5102c4244bada65fdce610546df76a178f4e2
Kim Se Jin <hollysj@naver.com>
2024-08-20 17:33:58 +0900

    20240820 로그인과 로그아웃 및 UserDetails 사용하기

--- secserver/src/main/java/org/example/controller/SecurityController.java
+++ secserver/src/main/java/org/example/controller/SecurityController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Log4j
 @RequestMapping("/security")
 @Controller
@@ -15,12 +17,24 @@ public class SecurityController {
         log.info("do all can access everybody");
     }
 
-    @GetMapping("/member") //MEMBER 또는 ADMIN 권한 필요
-    public void doMember() {
-        log.info("logined member");
-    }
+//    @GetMapping("/member") //MEMBER 또는 ADMIN 권한 필요
+//    public void doMember() {
+//        log.info("logined member");
+//    }
     @GetMapping("/admin") //ADMIN 권한 필요
     public void doAdmin() {
         log.info("admin only");
+    }
+    @GetMapping("/login")
+    public void login() {
+        log.info("login page");
+    }
+    @GetMapping("/logout")
+    public void logout() {
+        log.info("logout page");
+    }
+    @GetMapping("/member")
+    public void doMember(Principal principal){
+        log.info("username = " + principal.getName());
     }
 }
